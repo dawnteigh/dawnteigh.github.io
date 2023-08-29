c41b549c6a2a6b2960e4146253fb2589b865ecaf
Donte Ladatto <donte279@gmail.com>
2023-08-29 14:39:59 -0500

    contact link accessibility

--- client/src/components/Contact.js
+++ client/src/components/Contact.js
@@ -6,8 +6,14 @@ const Contact = () => {
       <h1>Contact</h1>
       <p>If you need someone for a job or are interested in networking, reach out to me on LinkedIn or shoot me an email:</p>
       <div>
-        <a id="profile-link" target="_blank" href="https://www.linkedin.com/in/donteladatto/"><i class="fa fa-linkedin-square" aria-hidden="true"></i></a>
-        <a href="mailto:donte279@gmail.com"><i class="fa-solid fa-envelope"></i></a>
+        <a alt="linkedin" target="_blank" rel="noreferrer" href="https://www.linkedin.com/in/donteladatto/">
+          <i class="fa fa-linkedin-square" aria-hidden="true"></i>
+          <span class="sr-only">LinkedIn</span>
+        </a>
+        <a alt="email" href="mailto:donte279@gmail.com">
+          <i class="fa-solid fa-envelope" aria-hidden="true"></i>
+          <span class="sr-only">Email</span>
+        </a>
       </div>
     </div>
   )
