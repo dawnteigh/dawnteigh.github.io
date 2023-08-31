936f632ca5cfe7ff15bcf484c85d0bdbae8c5cb1
Donte Ladatto <donte279@gmail.com>
2023-08-31 10:21:30 -0500

    add react-router-dom and client routes

--- client/src/App.js
+++ client/src/App.js
@@ -2,15 +2,18 @@ import Nav from './components/Nav';
 import Home from './components/Home';
 import Projects from './components/Projects';
 import Contact from './components/Contact';
+import { Route, Routes } from 'react-router-dom'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <Nav />
-      <Home />
-      <Projects />
-      <Contact />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/portfolio" element={<Projects />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
     </div>
   );
 }
