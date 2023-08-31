1f47784d70fa6716a9b9eeccfcc1db50f27fe110
Donte Ladatto <donte279@gmail.com>
2023-08-31 10:25:35 -0500

    utilize NavLinks in Nav

--- client/src/components/Nav.js
+++ client/src/components/Nav.js
@@ -1,11 +1,33 @@
 import React from 'react'
+import { NavLink } from 'react-router-dom'
 
 const Nav = () => {
   return (
     <nav id="navbar">
-      <a class="nav-link" href="#welcome-section">About</a>
-      <a class="nav-link" href="#projects">Portfolio</a>
-      <a class="nav-link" href="#contact">Contact</a>
+      <NavLink
+        to="/"
+        className={({ isActive, isPending }) =>
+          isPending ? "pending" : isActive ? "active" : ""
+        }
+      >
+        Home
+      </NavLink>
+      <NavLink
+        to="/portfolio"
+        className={({ isActive, isPending }) =>
+          isPending ? "pending" : isActive ? "active" : ""
+        }
+      >
+        Portfolio
+      </NavLink>
+      <NavLink
+        to="/contact"
+        className={({ isActive, isPending }) =>
+          isPending ? "pending" : isActive ? "active" : ""
+        }
+      >
+        Contact
+      </NavLink>
     </nav>
   )
 }
