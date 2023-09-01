8b1b1d6e8b3c99e680b5153898a35f025a44af2b
Donte Ladatto <donte279@gmail.com>
2023-09-01 16:50:58 -0500

    update contact page with more links

--- client/src/components/Bio.js
+++ client/src/components/Bio.js
@@ -4,7 +4,12 @@ const Bio = () => {
   return (
     <div id="bio">
       <h1>Hey, I'm Donte.</h1>
-      <p>I'm a full-stack web developer. I'm most well-versed with React + Ruby on Rails, but I have used other technologies as well. I can quickly get up to speed with any language or framework to meet project needs; same concepts, different syntax. Let's <a href="#contact">talk</a>.</p>
+      <p>
+        I'm a full-stack web developer. I'm most well-versed with React + Ruby on Rails, but I have used other technologies as well. I can quickly get up to speed with any language or framework to meet project needs; same concepts, different syntax. Let's <a href="/contact">talk</a>.
+      </p>
+      <p>
+        When I'm not glued to my computer, I enjoy cooking, playing tennis or disc golf, craft beer, and karaoke.
+      </p>
     </div>
   )
 }
--- client/src/components/Contact.js
+++ client/src/components/Contact.js
@@ -4,15 +4,31 @@ const Contact = () => {
   return (
     <div id="contact">
       <h1>Contact</h1>
-      <p>If you need someone for a job or are interested in networking, reach out to me on LinkedIn or shoot me an email:</p>
-      <div>
+      <p>If you're interested in networking, have a job posting for me to look at, or you just want to talk projects or code in general, reach out to me via email, LinkedIn, or Discord!</p>
+      <div id="icons">
+        <a alt="email" href="mailto:donte279@gmail.com">
+          <i className="fa-solid fa-envelope" aria-hidden="true"></i>
+          <span className="sr-only">Email</span>
+        </a>
         <a alt="linkedin" target="_blank" rel="noreferrer" href="https://www.linkedin.com/in/donteladatto/">
           <i className="fa fa-linkedin-square" aria-hidden="true"></i>
           <span className="sr-only">LinkedIn</span>
         </a>
-        <a alt="email" href="mailto:donte279@gmail.com">
-          <i className="fa-solid fa-envelope" aria-hidden="true"></i>
-          <span className="sr-only">Email</span>
+        <a alt="discord" target="_blank" rel="noreferrer" href="https://www.discord.com/users/dawnteigh">
+          <i className="fa-brands fa-discord"></i>
+          <span className="sr-only">Discord</span>
+        </a>
+        <a alt="free-code-camp" target="_blank" rel="noreferrer" href="https://www.freecodecamp.org/dladatto">
+          <i className="fa-brands fa-free-code-camp"></i>
+          <span className="sr-only">freeCodeCamp</span>
+        </a>
+        <a alt="github" target="_blank" rel="noreferrer" href="https://github.com/dawnteigh">
+          <i className="fa-brands fa-github"></i>
+          <span className="sr-only">GitHub</span>
+        </a>
+        <a alt="dev-blog" target="_blank" rel="noreferrer" href="https://dev.to/dawnteigh">
+          <i className="fa-brands fa-dev"></i>
+          <span className="sr-only">Blog</span>
         </a>
       </div>
     </div>
