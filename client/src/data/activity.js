ce55248266b4974425520775589da4474cd1e3bd
Donte Ladatto <donte279@gmail.com>
2023-09-04 17:48:23 -0500

    video player responsiveness + first blog entry; deploy-ready

--- client/src/data/activity.js
+++ client/src/data/activity.js
@@ -1,22 +1,6 @@
 export const activity = [
   {
-    date: 'September 1st, 2023',
-    entry: "I did a thing and learned a thing.I did a thing and learned a thing.I did a thing and learned a thing.I did a thing and learned a thing.I did a thing and learned a thing.I did a thing and learned a thing.I did a thing and learned a thing.I did a thing and learned a thing.I did a thing and learned a thing."
+    date: 'September 4th, 2023',
+    entry: "Finally put the finishing touches on this little portfolio site. Spent a lot of time agonizing over CSS, but I feel that it was worth it. The next time I undertake something similar, I'll be able to move a lot faster. I certainly became stronger at using flexbox! Programmatically disabling scroll using component state (when rendering the video player) was something new for me as well. Site goes live tonight!"
   },
-  {
-    date: 'September 1st, 2023',
-    entry: "I did a thing and learned a thing.I did a thing and learned a thing.I did a thing and learned a thing.I did a thing and learned a thing.I did a thing and learned a thing.I did a thing and learned a thing.I did a thing and learned a thing.I did a thing and learned a thing."
-  },
-  {
-    date: 'September 1st, 2023',
-    entry: "I did a thing and learned a thing.I did a thing and learned a thing.I did a thing and learned a thing.I did a thing and learned a thing.I did a thing and learned a thing.I did a thing and learned a thing.I did a thing and learned a thing.I did a thing and learned a thing."
-  },
-  {
-    date: 'September 1st, 2023',
-    entry: "I did a thing and learned a thing.I did a thing and learned a thing.I did a thing and learned a thing.I did a thing and learned a thing.I did a thing and learned a thing.I did a thing and learned a thing.I did a thing and learned a thing."
-  },
-  {
-    date: 'September 1st, 2023',
-    entry: "I did a thing and learned a thing.I did a thing and learned a thing.I did a thing and learned a thing.I did a thing and learned a thing.I did a thing and learned a thing.I did a thing and learned a thing."
-  }
 ]
