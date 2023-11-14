93b8f2185b8b74cea9941cb1e71dc62bfc30b8ce
Donte Ladatto <donte279@gmail.com>
2023-11-13 21:41:23 -0600

    wrap app in AnimatePresence

--- src/App.js
+++ src/App.js
@@ -4,18 +4,21 @@ import Projects from './components/Projects';
 import Contact from './components/Contact';
 import TaskBoard from './components/TaskBoard';
 import { Route, Routes } from 'react-router-dom'
+import { AnimatePresence } from 'framer-motion';
 import './App.css';
 
 function App() {
   return (
     <div>
       <Nav />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/portfolio" element={<Projects />} />
-        <Route path="/contact" element={<Contact />} />
-        <Route path="/kanban" element={<TaskBoard />} />
-      </Routes>
+      <AnimatePresence mode="wait">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/portfolio" element={<Projects />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/kanban" element={<TaskBoard />} />
+        </Routes>
+      </AnimatePresence>
     </div>
   );
 }
